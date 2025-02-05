58067554c4022e16aa9b9567c455169998155165
Jeffrey <kingtingthegreat@gmail.com>
2025-02-05 13:26:02 -0500

    vercel font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { JetBrains_Mono } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "CS391 File Generator",
   description:
     "Submit your Vercel and GitHub links here to create a text file to submit on Gradescope.",
 };
+
+const jetBrainsMono = JetBrains_Mono({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -14,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={jetBrainsMono.className}>{children}</body>
     </html>
   );
 }
